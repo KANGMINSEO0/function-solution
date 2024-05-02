ecf22d9885d2a3fef5bf128986189809979a2ffe
KANGMINSEO0 <juhee0629032@gmail.com>
2024-05-02 22:54:46 +0900

    section03/Practice3-문제16기능개발

---
+++ src/main/java/com/kms/section03/Practice3.java
@@ -0,0 +1,10 @@
+package com.kms.section03;
+
+public class Practice3 {
+    // 문제16 기능개발
+
+    public static void main(String[] args) {
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+    }
+}
