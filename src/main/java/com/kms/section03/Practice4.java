2d8190f6949b686b25da7d733ef467c703bc14a2
KANGMINSEO0 <juhee0629032@gmail.com>
2024-05-30 22:26:32 +0900

    section03/Practice4

--- src/main/java/com/kms/section03/Practice4.java
+++ src/main/java/com/kms/section03/Practice4.java
@@ -1,6 +1,29 @@
 package com.kms.section03;
 
+import java.util.ArrayDeque;
+import java.util.Arrays;
+
 public class Practice4 {
 
     // 문제 17 카드 뭉치
+    /* 다음과 같은 규칙으로 카드에 적힌 단어들을 사용해 원하는 순서의 단어 배열을 만들 수 있는지 알고 싶음
+    *
+    *  원하는 카드 뭉치에서 카드를 순서대로 한 장씩 사용
+    *  한 번 사용한 카드는 다시 사용할 수 없음
+    *  카드를 사용하지 않고 다음 카드로 넘어갈 수 없음
+    *  기존에 주어진 카드 뭉치의 단어 순서는 바꿀 수 없음
+    *
+    * */
+
+    public String solution(String[] cards1, String[] cards2, String[] goal) {
+
+        // cards와 goal을 deque로 변환
+        ArrayDeque<String> cardsDeque1 = new ArrayDeque<>(Arrays.asList(cards1));
+        ArrayDeque<String> cardsDeque2 = new ArrayDeque<>(Arrays.asList(cards2));
+        ArrayDeque<String> goalDeque = new ArrayDeque<>(Arrays.asList(goal));
+
+        // goalDeque에 문자열이 남아 있으면 계속 반복
+
+        return "";
+    }
 }
