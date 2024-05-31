33817c300f2b6d3bff4a001e66484af51955d936
KANGMINSEO0 <juhee0629032@gmail.com>
2024-05-31 22:54:09 +0900

    section04/Practice1-basic

---
+++ src/main/java/com/kms/section04/Practice1.java
@@ -0,0 +1,13 @@
+package com.kms.section04;
+
+import java.util.HashMap;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        // HashMap<KeyType, ValueType> 입니다.
+        HashMap<String, Integer> hashMap = new HashMap<>();
+
+        // hashMap에 데이터 추가
+        hashMap.put("ABC", 10);
+    }
+}
