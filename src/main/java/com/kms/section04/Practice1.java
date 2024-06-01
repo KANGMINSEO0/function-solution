1b122508f8fb3a64ba55ee54b8f923770e101176
KANGMINSEO0 <juhee0629032@gmail.com>
2024-06-01 21:44:12 +0900

    section04/Pracitce1, Practice2

--- src/main/java/com/kms/section04/Practice1.java
+++ src/main/java/com/kms/section04/Practice1.java
@@ -9,5 +9,18 @@ public class Practice1 {
 
         // hashMap에 데이터 추가
         hashMap.put("ABC", 10);
+        hashMap.put("BBB", 20);
+        hashMap.put("AAA", 30);
+        hashMap.put("ABC", 15);
+
+        System.out.println(hashMap.isEmpty());  // false
+        System.out.println(hashMap.get("ABC")); // 15
+        System.out.println(hashMap.containsKey("ABC")); // true
+
+        hashMap.remove("ABC");  // hashMap에서 키가 "ABC"인 데이터 제거
+        System.out.println(hashMap.size()); // 2
+
+        hashMap.clear();    // 해시맵의 모든 데이터 삭제
+        System.out.println(hashMap.isEmpty());  // true
     }
 }
---
+++ src/main/java/com/kms/section04/Practice2.java
@@ -0,0 +1,7 @@
+package com.kms.section04;
+
+public class Practice2 {
+
+    // 문18 두 개의 수로 특정ㄱ밧 만들기
+    
+}
