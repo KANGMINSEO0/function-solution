95a3a2717034ca522bc184a877a26f02a883a71b
KANGMINSEO0 <juhee0629032@gmail.com>
2024-06-02 21:18:48 +0900

    section04/Practice2 - 문18 두 개의 수로 특정값 만들기

--- src/main/java/com/kms/section04/Practice2.java
+++ src/main/java/com/kms/section04/Practice2.java
@@ -1,7 +1,30 @@
 package com.kms.section04;
 
-public class Practice2 {
+import java.util.HashMap;
+import java.util.HashSet;
 
-    // 문18 두 개의 수로 특정ㄱ밧 만들기
-    
+public class Practice2 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 8};
+        int target = 6;
+
+        System.out.println(solution(arr, target));
+    }
+
+    // 문18 두 개의 수로 특정값 만들기
+
+    public static boolean solution(int[] arr, int target) {
+        HashSet<Integer> hashMap = new HashSet<>();
+
+        for (int i : arr) {
+            // target에서 현재 원소를 뺀 값이 해시셋 있는지 확인
+            if (hashMap.contains(target - i)) {
+                return true;
+            }
+            // 해시셋 현재 값 저장
+            hashMap.add(i);
+        }
+        return false;
+    }
+
 }
