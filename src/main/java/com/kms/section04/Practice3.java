fc6067cb4650d77a06414174901571cca81a7018
KANGMINSEO0 <juhee0629032@gmail.com>
2024-06-04 22:03:01 +0900

    section04/Practine3 - 문제 19 완주 못한 선수

--- src/main/java/com/kms/section04/Practice3.java
+++ src/main/java/com/kms/section04/Practice3.java
@@ -3,5 +3,10 @@ package com.kms.section04;
 public class Practice3 {
 
     // 문19 완주하지 못한 선수
-    
+
+    public static String solution(String[] participant, String[] completion) {
+
+        return "";
+    }
+
 }
