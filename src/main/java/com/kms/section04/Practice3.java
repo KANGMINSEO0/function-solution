f4268c02aed7e2494abac96690453ce9f2c4464d
KANGMINSEO0 <juhee0629032@gmail.com>
2024-06-06 22:58:22 +0900

    section04/Practice3

--- src/main/java/com/kms/section04/Practice3.java
+++ src/main/java/com/kms/section04/Practice3.java
@@ -1,10 +1,19 @@
 package com.kms.section04;
 
+import java.util.HashMap;
+
 public class Practice3 {
 
     // 문19 완주하지 못한 선수
 
     public static String solution(String[] participant, String[] completion) {
+
+        // 해시맵 생성
+        HashMap<String, Integer> hashMap = new HashMap<String, Integer>();
+        // 완주한 선두들의 이름을 해시맵에 저장
+        for (String string : completion) {
+            hashMap.put(string, hashMap.getOrDefault(string, 0) + 1);
+        }
 
         return "";
     }
