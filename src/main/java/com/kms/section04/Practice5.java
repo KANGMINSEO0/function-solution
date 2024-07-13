cb50fca43b22b2bedbe1bfe9ee7771b63725be3f
KANGMINSEO0 <juhee0629032@gmail.com>
2024-07-13 22:38:33 +0900

    문21 오픈 채팅방

---
+++ src/main/java/com/kms/section04/Practice5.java
@@ -0,0 +1,16 @@
+package com.kms.section04;
+
+import java.util.HashMap;
+
+public class Practice5 {
+
+    // 문21 오픈 채팅방
+    public static void main(String[] args) {
+
+    }
+
+    private String[] solution(String[] record) {
+        HashMap<String, Integer> map = new HashMap<>();
+        return record;
+    }
+}
