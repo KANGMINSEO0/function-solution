e5c1c5449992c5b72d4f104cfe8aaaa020985109
KANGMINSEO0 <juhee0629032@gmail.com>
2024-07-27 18:04:08 +0900

    section05/Practice1

--- src/main/java/com/kms/section05/Practice1.java
+++ src/main/java/com/kms/section05/Practice1.java
@@ -4,6 +4,11 @@ public class Practice1 {
 
     // 문제 25 트리순회
     public static void main(String[] args) {
-        
+
+    }
+
+    public static String[] solution(int[] nodes) {
+
+        return new String[0];
     }
 }
