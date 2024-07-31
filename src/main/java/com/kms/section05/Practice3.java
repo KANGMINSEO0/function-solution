182ec6e9c4fe134e5ca0be8d268128e38ef9cde4
KANGMINSEO0 <juhee0629032@gmail.com>
2024-07-31 17:28:58 +0900

    문27

---
+++ src/main/java/com/kms/section05/Practice3.java
@@ -0,0 +1,19 @@
+package com.kms.section05;
+
+public class Practice3 {
+
+    // 문27 다단계 칫솔 판매
+    public static void main(String[] args) {
+
+        String[] enroll = {"john", "mary", "edward", "sam", "emily", "jaimie", "tod", "young"}; // 현재 판매원
+        String[] referral = {"-", "-", "mary", "edward", "mary", "mary", "jaimie", "edward"};   // 추천자
+        String[] seller = {"young", "john", "tod", "emily", "mary"};    // 판매원
+        int[] amount = {12, 4, 2, 5, 10};
+
+    }
+
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+
+        return amount;
+    }
+}
