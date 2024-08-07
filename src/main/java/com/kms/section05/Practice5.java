59c692961bf39f25158439188d839a0383aff1a0
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-07 15:36:57 +0900

    문29 길 찾기 게임

---
+++ src/main/java/com/kms/section05/Practice5.java
@@ -0,0 +1,20 @@
+package com.kms.section05;
+
+public class Practice5 {
+
+    // 문제 29 길 찾기 게임
+
+    private static class Node {
+        int x, y, num;      // 노드의 좌표, 번호 저장
+        Node left, right;   // 노드의 왼쪽, 오른쪽 자식 노드
+
+
+        public Node(int num, int x, int y) {
+            this.num = num;
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    // 이진 트리 생성 메서드
+}
