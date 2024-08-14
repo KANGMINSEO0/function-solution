25c7ebd7883f66c1ddb4a8235df5431f03a22d6c
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-14 15:54:50 +0900

    문31

---
+++ src/main/java/com/kms/section06/Practice2.java
@@ -0,0 +1,14 @@
+package com.kms.section06;
+
+public class Practice2 {
+
+    // 문제 31 포켓몬
+    public static void main(String[] args) {
+
+    }
+
+    public static int solution(int[] nums) {
+
+        return 0;
+    }
+}
