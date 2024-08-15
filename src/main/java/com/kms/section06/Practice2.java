874c2f51bbfa222375cf20d65998663e5ecc025b
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-15 17:36:09 +0900

    문 31

--- src/main/java/com/kms/section06/Practice2.java
+++ src/main/java/com/kms/section06/Practice2.java
@@ -1,5 +1,9 @@
 package com.kms.section06;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.Collectors;
+
 public class Practice2 {
 
     // 문제 31 포켓몬
@@ -9,6 +13,8 @@ public class Practice2 {
 
     public static int solution(int[] nums) {
 
+        // nums 리스트에서 중복을 제거한 집합(set)을 구함
+        HashSet<Integer> set = Arrays.stream(nums).boxed().collect(Collectors.toCollection(HashSet::new));
         return 0;
     }
 }
