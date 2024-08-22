80dbc24b25679fbd5e0b8115800ec3ad839c708f
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-22 20:16:27 +0900

    문33

---
+++ src/main/java/com/kms/section06/Practice4.java
@@ -0,0 +1,15 @@
+package com.kms.section06;
+
+public class Practice4 {
+
+    // 문33 섬 연결하기
+    /*
+    * n개의 섬 사이에 다리를 건설하는 비용 costs가 주어질 때 최소 비용으로 모든 섬이 서로 통행
+    * 다리를 여러 번 건너더라도 목표 지점에 도달할 수만 있으면 통행할 수 있다고 봄
+    * ex) A, B 섬 사이에 다리가 있고, B, C 섬 사이에 다리가 있으면 A, C 섬은 서로 동행 가능
+    * */
+
+    public static void main(String[] args) {
+
+    }
+}
