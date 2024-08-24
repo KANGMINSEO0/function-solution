e7c9cde732159ee9f789fb695faa313d93f07ba6
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-24 12:32:14 +0900

    문33

--- src/main/java/com/kms/section06/Practice4.java
+++ src/main/java/com/kms/section06/Practice4.java
@@ -12,4 +12,26 @@ public class Practice4 {
     public static void main(String[] args) {
 
     }
+
+    private static int[] parent;
+
+    private static int find(int x) {
+        // x가 속한 집합의 루트 노드 찾기
+        if (parent[x] == x)
+            return x;
+        // 경로 압축 :  x의 부모를 루트로 설정
+        return parent[x] = find(parent[x]);
+    }
+
+    private static void union(int x, int y) {
+        // 두 집합을 하나의 집합으로 합치기
+        int root1 = find(x);
+        int root2 = find(y);
+        parent[root2] = root1;
+    }
+
+    public static int solution(int n, int[][] costs) {
+
+        return n;
+    }
 }
