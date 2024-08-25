e46b208684734e52f4094cd3ae29e4a2aee4db2a
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-25 15:34:53 +0900

    문33 섬연결하기

--- src/main/java/com/kms/section06/Practice4.java
+++ src/main/java/com/kms/section06/Practice4.java
@@ -1,5 +1,7 @@
 package com.kms.section06;
 
+import java.util.Arrays;
+
 public class Practice4 {
 
     // 문33 섬 연결하기
@@ -11,6 +13,12 @@ public class Practice4 {
 
     public static void main(String[] args) {
 
+        int n = 4;
+        int[][] costs = {{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}};
+
+        int result = solution(n, costs);
+
+        System.out.println(result);
     }
 
     private static int[] parent;
@@ -32,6 +40,33 @@ public class Practice4 {
 
     public static int solution(int n, int[][] costs) {
 
-        return n;
+        // 비용을 기준으로 다리를 오츰차순 정력
+        Arrays.sort(costs, (o1, o2) -> Integer.compare(o1[2], o2[2]));
+
+        // parent 배열 초기화
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        int answer = 0; // 최소 신장 트리의 총비용
+        int edges = 0;  // 연결된 다리의 수
+
+        for (int[] edge : costs) {
+            // n - 1 개의 다리가 연결된 경우 모든 셀이 연결됨
+            if (edges == n - 1)
+                break;
+
+            // 현재 다리가 연결하는 두 섬이 이미 연결되어 있는지 확인
+            if (find(edge[0]) != find(edge[1])) {
+                // 두 섬을 하나의 집합으로 연결함
+                union(edge[0], edge[1]);
+                // 현재 다리의 건설 비용을 총비용에 추가
+                answer += edge[2];
+                // 사용된 다리의 수 1증가
+                edges++;
+            }
+        }
+        return answer;
     }
 }
