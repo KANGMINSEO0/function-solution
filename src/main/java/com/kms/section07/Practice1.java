dab1161406fa84d807cb150196731dc2fb214763
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-27 20:03:32 +0900

    문34 깊이 우선 탐색 순회

--- src/main/java/com/kms/section07/Practice1.java
+++ src/main/java/com/kms/section07/Practice1.java
@@ -1,6 +1,7 @@
 package com.kms.section07;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Practice1 {
 
@@ -20,6 +21,8 @@ public class Practice1 {
         int[][] graph = {{1, 2}, {2, 3}, {3, 4}, {4, 5}};
         int start = 1;
         int n = 5;
+        int[] result = solution(graph, start, n);
+        System.out.println(Arrays.toString(result));
     }
 
     // 인접 리스트 저장할 ArrayList 배열
@@ -31,6 +34,34 @@ public class Practice1 {
 
     private static int[] solution(int[][] graph, int start, int n) {
 
-        return new int[0];
+        // 인접 리스트 초기화
+        adjList = new ArrayList[n + 1];
+        for (int i = 0; i < adjList.length; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+
+        // 그래프를 인접 리스트로 변환
+        for (int[] edge : graph) {
+            adjList[edge[0]].add(edge[1]);
+        }
+
+        // DFS를 순회한 결과를 반환
+        visited = new boolean[n + 1];
+        answer = new ArrayList<>();
+        dfs(start); // 시작 노드에서 깊이 우선 탐색 시작
+
+        // DFS 탐색 결과 반환
+        return answer.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    private static void dfs(int now) {
+        visited[now] = true;    // 현재 노드를 방문했음을 저장
+        answer.add(now);    // 현재 노드를 결과 리스트에 추가
+        // 현재 노드와 인접한 노드 순회
+        for (int next : adjList[now]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
     }
 }
