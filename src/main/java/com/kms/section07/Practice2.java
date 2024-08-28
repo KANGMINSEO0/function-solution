c088c9fd2cccab1ffb6163438ada2c3d9b55f0bc
KANGMINSEO0 <juhee0629032@gmail.com>
2024-08-28 20:16:54 +0900

    문 35

---
+++ src/main/java/com/kms/section07/Practice2.java
@@ -0,0 +1,16 @@
+package com.kms.section07;
+
+public class Practice2 {
+
+    // 문 35 너비 우선 탐색 순회
+    /*
+    *
+    * 노드의 최대 개수는 100개
+    * 시작 노드부터 시작해서 모든 노드를 방문할 수 있는 경로가 항상 있음
+    * 그래프의 노드는 숫자
+    *
+    * */
+    public static void main(String[] args) {
+        
+    }
+}
