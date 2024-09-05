b1105f6f1e43ce9fda5d0d558be5d3b2b3c9dc48
KANGMINSEO0 <juhee0629032@gmail.com>
2024-09-05 20:22:54 +0900

    수정

--- src/main/java/com/kms/section07/Practice3.java
+++ src/main/java/com/kms/section07/Practice3.java
@@ -56,7 +56,7 @@ public class Practice3 {
             // 만약 현재 노드의 거리 값이 큐에서 가져온 거리 값보다 크면, 해당 노드는 이미 방문한 것이므로 무시
             if (dist[now.dest] < now.cost)
                 continue;
-            // 현재 녿와 인접한 노드들의 거리 값을 계산하여 업데이트
+            // 현재 노드와 인접한 노드들의 거리 값을 계산하여 업데이트
             for (Node next : adjList[now.dest]) {
                 // 기존에 발견했던 거리보다 더 짧은 거리를 발견하면 거리 값을 갱신하고 큐에 넣음
                 if (dist[next.dest] > next.cost) {
