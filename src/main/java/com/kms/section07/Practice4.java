45c9752ff6812859b2aacd5d0c373992619042f0
KANGMINSEO0 <juhee0629032@gmail.com>
2024-09-08 16:54:14 +0900

    문37 게임 맵 최단 거리

--- src/main/java/com/kms/section07/Practice4.java
+++ src/main/java/com/kms/section07/Practice4.java
@@ -6,6 +6,7 @@ public class Practice4 {
 
     public static void main(String[] args) {
 
+        int[][] maps = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
     }
 
     
