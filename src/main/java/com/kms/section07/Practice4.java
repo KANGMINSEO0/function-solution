f2c5822806d1fb7af63bd5a58464c26b5063164b
KANGMINSEO0 <juhee0629032@gmail.com>
2024-09-11 20:11:41 +0900

    문 37

--- src/main/java/com/kms/section07/Practice4.java
+++ src/main/java/com/kms/section07/Practice4.java
@@ -9,5 +9,13 @@ public class Practice4 {
         int[][] maps = {{1, 0, 1, 1, 1}, {1, 0, 1, 0, 1}, {1, 0, 1, 1, 1}, {1, 1, 1, 0, 1}, {0, 0, 0, 0, 1}};
     }
 
+    // 이동할 수 있는 방향을 나타내는 배열 rx, ry 선언
+    private static final int[] rx = {0, 0, 1, -1};
+    private static final int[] ry = {1, -1, 0, 0};
+
+    private static class Node {
+        
+    }
+
     
 }
